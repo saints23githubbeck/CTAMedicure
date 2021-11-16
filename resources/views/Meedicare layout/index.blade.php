63be744b8f0e49341099d8a6b165a9b2b9ba5305
Morvic-Scp <72229226+Morvic-Scp@users.noreply.github.com>
2021-11-16 14:15:15 -0800

    Add files via upload

---
+++ resources/views/Meedicare layout/index.blade.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/all.min.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Login</title>
+</head>
+<body>
+    <div class="menu">
+        <nav>
+                <div class="logo"><h1><strong>MEDICURE</strong></h1></div>
+                <ul id="menuul" class="col-lg-8 col-md-8">
+                <li class="nav-item">
+                    <a href="#">HOME</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#">CONSULT A DOCTOR</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#">BUY A PRESCRIPTION</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#">KNOWLEDGE BANK</a>
+                </li>
+                <li>
+                    <a href="#" class="btn btn-danger">SIGN IN</a>
+                </li>
+                </ul>
+        </nav>
+    </div>
+    <div class="hero d-flex align-items-end">
+        <div style="padding-bottom: 4rem;" class="px-5 col-lg-5">
+            <h1 class="mb-3">BIG TITLE</h1>
+            <p class="pb-4">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempora, quaerat. Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam atque modi, harum neque laborum quos aperiam quia, ullam alias tenetur dolorem fugiat ipsum maxime mollitia? Quibusdam est amet aperiam at.</p>
+            <div class="">
+               <strong><a style="font-weight: bold;" href="#" class="btn btn-danger">Learn more</a></strong> 
+                <strong><a style="font-weight: bold;" href="#" class="btn btn-light text-danger">Register</a></strong>
+            </div>
+        </div>
+    </div>
+    <section>
+        <div class="d-flex">
+            <div class="col-lg-5 subhero"><img src="./Sources/Rectangle_1296@2x.png" width="100%" alt=""></div>
+            <div class="col-lg-8 card-body pt-5">
+                <h3 style="padding-top: 4rem;" >Lorem ipsum dolor sit amet consectetur, adipisicing elit. Alias earum nobis quis iste.</h3>
+                <div class="row pt-5">
+                    <div class="col-lg-6 subitem">
+                        <i class="fas fa-tv fa-3x"></i>
+                        <div class="">
+                            <h3>Lorem ipsum dolor sit amet.</h3>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae voluptate error assumenda iste laudantium excepturi hic beatae expedita maiores dolorum.</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 subitem">
+                        <i class="fas fa-tag pe-2 fa-3x"></i>
+                        <div class="">
+                            <h3>Lorem ipsum dolor sit amet.</h3>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae voluptate error assumenda iste laudantium excepturi hic beatae expedita maiores dolorum.</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 subitem">
+                        <i class="fas fa-assistive-listening-systems pe-2 fa-3x"></i>
+                        <div class="">
+                            <h3>Lorem ipsum dolor sit amet.</h3>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae voluptate error assumenda iste laudantium excepturi hic beatae expedita maiores dolorum.</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 subitem">
+                        <i class="fas fa-crown pe-2 fa-3x"></i>
+                        <div class="">
+                            <h3>Lorem ipsum dolor sit amet.</h3>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae voluptate error assumenda iste laudantium excepturi hic beatae expedita maiores dolorum.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section>
+        <div style="padding-top: 4rem;" class="text-center">
+            <h2>Our Services</h2>
+        </div>
+
+    </section>
+
+    <script src="js/jquery-3.2.1.slim.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+</body>
+</html>
