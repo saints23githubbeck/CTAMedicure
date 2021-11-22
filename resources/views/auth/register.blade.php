85527e6a313817cd50dc1d158d9822b3d9c53971
James <jamesonyemi@gmail.com>
2021-11-22 16:10:32 +0000

    Authentucation pages added

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,77 +1,95 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
-
-                                @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
+<section class="mt-5 d-flex justify-content-center">
+    <div class="container justify-content-center align-items-center">
+      <div class="row justify-content-center align-items-center h-100">
+        <div class="col-lg-7 col-sm-12">
+          <div class="my-1 card card-registration">
+            <div class="row g-0">
+              <div class="col-lg-6 col-sm-6 mw-100 d-sm-none d-md-block d-none d-lg-block d-md-none d-lg-none d-xl-block">
+                <img
+                  src="{{ asset('Sources/signInimage.jpg') }}"
+                  alt="Sample photo"
+                  loading="lazy"
+                  class="img-fluid h-100 w-100"
+                />
+              </div>
+              <div class="col-lg-6 col-sm-12 col-md-12 mw-100">
+                <form method="POST" action="#">
+                    @csrf
+                    <div class="text-black card-body p-md-4">
+                        <div class="row align-items-center">
+                            <h5 class="mb-5 text-center text-uppercase text-bold text-primary">sign up to medicare</h5>
                         </div>
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+                    <div class="mb-1 col-12 form-outline">
+                        <label class="form-label" for="form3Example97">Name</label>
+                            <input
+                                type="text"
+                                id="name"
+                                class="form-control form-control-sm @error('name') is-invalid @enderror" name="name"
+                                value="{{ old('name') }}"
+                                required
+                                autocomplete="name"
+                                autofocus
+                            />
+                            @error('name')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                    </div>
+                    <div class="mb-1 col-12 form-outline">
+                        <label class="form-label" for="form3Example97">Email</label>
+                        <input
+                        type="email"
+                        id="email"
+                        class="form-control form-control-sm @error('email') is-invalid @enderror" email="email"
+                        value="{{ old('email') }}"
+                        required
+                        autocomplete="email"
+                    />
+                    @error('email')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                    </div>
+                    <div class="mb-1 col-12 form-outline">
+                        <label class="form-label" for="form3Example97">Password</label>
+                        <input
+                        type="password"
+                        id="password"
+                        class="form-control form-control-sm @error('password') is-invalid @enderror" password="password"
+                        value="{{ old('password') }}"
+                        required
+                        autocomplete="password"
+                        autofocus
+                    />
+                    @error('password')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                    </div>
+                  <div class="gap-2 mt-4 d-grid p-sm-5 p-lg-0">
+                    <button type="submit" class="btn btn-primary" >Sign Up</button>
+                  </div>
+                  <div class="row justify-content-sm-center">
+                    <div class="mx-5 mt-2 mb-2 text-center">
+                        <p class="mt-2 mb-0"><small class="mr-2 text-dark">Already have an account ?</small>
+                            <a href="{{ route("login") }}" class="text-primary text-decoration-none font-weight-bold">Sign in
+                            </a>
+                        </p>
+                    </div>
                 </div>
+                </div>
+                </form>
+              </div>
             </div>
+          </div>
         </div>
+      </div>
     </div>
-</div>
+  </section>
 @endsection
--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,26 +1,27 @@
 <nav class="navbar navbar-expand-lg navbar-light cta-bg-primary">
-    <a class="navbar-brand text-white" href="#"> Medicure</a>
-    <button class="navbar-toggler cta-bg-primary text-white" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <a class="text-white navbar-brand" href="#"> Medicure</a>
+    <button class="text-white navbar-toggler cta-bg-primary" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon navbar-light"></span>
     </button>
 
     <div class="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
 
-        <div class="navbar-nav text-center">
+        <div class="text-center navbar-nav">
             <li class="nav-item">
-                <a class="nav-link text-white hvr-underline-from-center" href="#">Home</a>
+                <a class="text-white nav-link hvr-underline-from-center" href="#">Home</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link text-white hvr-underline-from-center" href="#">Consult Doctor</a>
+                <a class="text-white nav-link hvr-underline-from-center" href="#">Consult Doctor</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link text-white hvr-underline-from-center" href="#">Buy Prescription</a>
+                <a class="text-white nav-link hvr-underline-from-center" href="#">Buy Prescription</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link text-white hvr-underline-from-center" href="#">Knowledge Bank</a>
+                <a class="text-white nav-link hvr-underline-from-center" href="#">Knowledge Bank</a>
             </li>
             <li class="nav-item">
-                <a class="btn btn-danger nav-link text-white bg-danger hvr-underline-from-center border-success" href="#">Sign-Up</a>
+                <a class="text-white btn btn-danger nav-link bg-danger hvr-underline-from-center border-success"
+                href="{{ route("register") }}">Sign-Up</a>
             </li>
         </div>
     </div>
