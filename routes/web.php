72382339ba3c175b1caa60d61b538b6d52a77508
arthurshadrack <arthurshadrack45@gmail.com>
2021-11-30 18:21:10 +0000

    suce

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\DashboardController;
+//use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\HomeController;
 
 use App\Http\Controllers\AdminPortal\DashboardController;
