6fb63ec2b63bb6bdd803c396671ad0231a74c3b0
daviddeladem <33095955+daviddeladem@users.noreply.github.com>
2021-11-30 15:21:26 +0000

    completed dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\HomeController;
-
-use App\Http\Controllers\AdminPortal\DashboardController;
-
 
 /*
 |--------------------------------------------------------------------------
@@ -27,26 +23,6 @@ Auth::routes();
 
 Route::get('/home', [HomeController::class, 'index'])->name('home');
 
-
 Route::get('/dashboard', function () {
-<<<<<<< HEAD
     return view('admin.dashboard');
 })->name('dashboard');
-=======
-    return view('admin.pages.dashboard');
-})->name('dashboard');
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Route::prefix('admin')->group(function () {
-
-    Route::get('/dashboard', DashboardController::class)->name("dashboard");
-});
-
-
-<<<<<<< Updated upstream
-=======
->>>>>>> a4d47239c1995131286d5ea522e413b8dfd749bd
->>>>>>> Stashed changes
