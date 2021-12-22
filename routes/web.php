11e0ecdcb762023d486b5c34c7461b34ff8e1e7b
Imtiaz-sohel <imtiazsohel93@gmail.com>
2021-12-22 23:07:12 +0600

    route

--- routes/web.php
+++ routes/web.php
@@ -20,34 +20,128 @@ Route::get('/', function () {
     return view('pages.index');
 });
 
-//Route::get('/dashboard', function () {
-//    return view('admin.dashboard');
-//})->name('dashboard');
+Route::get('/payment', function () {
+    return view('order.payment');
+});
+Route::get('/order', function () {
+    return view('order.order');
+});
+Route::get('/request', function () {
+    return view('order.incoming_request');
+});
+Auth::routes();
+
+
+Route::get('/delivery', function () {
+   return view('admin.pages.delivery');
+})->name('delivery');
+
+
+Route::get('/completed/delivery', function () {
+    return view('admin.pages.completed_delivery');
+ })->name('completed_delivery');
+
+
+Route::get('/doctor_list', function () {
+    return view('admin.pages.doctor_list');
+ })->name('doctor_list');
+
+ Route::get('/order_status', function () {
+    return view('admin.pages.order_status');
+ })->name('order_status');
+
+ Route::get('/pharmacy_list', function () {
+    return view('admin.pages.pharmacy_list');
+ })->name('pharmacy_list');
+
+
+Route::get('/dashboard', function () {
+   return view('admin.dashboard');
+})->name('dashboard');
+
 
 Route::get('/location', function () {
-   return view('admin.location');
+    return view('admin.location');
+
+});
+Route::get('/dashboard', function () {
+    return view('admin.dashboard');
+})->name('dashboard');
+
+
+ Route::get('/detail/modal', function () {
+    return view('admin.pages.detail_modal');
+ })->name('detail_modal');
+
+ Route::get('/orderlist', function () {
+    return view('admin.pages.orderlist');
+ })->name('orderlist');
+
+ Route::get('/billing_info', function () {
+    return view('admin.pages.billing_info');
+ })->name('billing_info');
+
+
+
+Route::get('/location', function () {
+   return view('admin.pages.location');
+
 })->name('location');
 
 Route::get('/prescription', function () {
     return view('admin.prescription');
 })->name('prescription');
 
+
+Route::get('/location', function () {
+   return view('admin.location');
+
+})->name('location');
+
+Route::get('/prescription', function () {
+    return view('admin.prescription');
+})->name('prescription');
+
+//return view('admin.pages.location');
+//})->name('location');
 Route::get('/appointments', function () {
     return view('admin.pages.appointment');
 })->name('appointments');
-
+//
 Route::get('/prescriptions', function () {
     return view('admin.pages.prescription');
 })->name('prescriptions');
-
+//
 Route::get('/users', function () {
     return view('admin.pages.user');
 })->name('users');
 
+
+Route::get('/roles', function () {
+    return view('admin.pages.role');
+
+})->name('roles');
+
+Route::get('/incoming', function () {
+    return view('admin.order.incoming_request');
+
+})->name('income');
+
+Route::get('/payment', function () {
+    return view('admin.order.payment');
+
+})->name('payment');
+
+Route::get('/order', function () {
+    return view('admin.order.order');
+
+})->name('order');
+
+
 Route::get('/request-list', function () {
     return view('admin.pages.request-list');
 })->name('requestList');
-
+//
 Route::prefix('admin')->group(function () {
 
 
@@ -69,4 +163,3 @@ Route::prefix('admin')->group(function () {
     });
 });
 
-Auth::routes();
