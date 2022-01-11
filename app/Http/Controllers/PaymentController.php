51b38fc7e04eec386895598993349e62bd4eb860
Sujon Ahammed <mimobaydullah@gmail.com>
2022-01-11 15:50:00 +0600

    git checkout

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -249,14 +249,7 @@ class PaymentController extends Controller
         else{
             return redirect()->route('pres')->with('add','Payment Failed');
         }
-        // Get the transaction from your DB using the transaction reference (txref)
-        // Check if you have previously given value for the transaction. If you have, redirect to your successpage else, continue
-        // Confirm that the currency on your db transaction is equal to the returned currency
-        // Confirm that the db transaction amount is equal to the returned amount
-        // Update the db transaction record (including parameters that didn't exist before the transaction is completed. for audit purpose)
-        // Give value for the transaction
-        // Update the transaction to note that you have given value for the transaction
-        // You can also redirect to your success page from here
+
 
     }
 
