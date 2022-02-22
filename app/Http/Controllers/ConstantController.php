3f2bd000188d7e470689a0be44951bc78c52b2cb
developerhuzaifaahmed11981 <developerhuzaifa11981@gmail.com>
2022-02-22 14:43:58 +0600

    a

--- app/Http/Controllers/ConstantController.php
+++ app/Http/Controllers/ConstantController.php
@@ -6,144 +6,117 @@ use Illuminate\Http\Request;
 use App\Models\Constant_settings;
 use App\Models\User;
 use App\Models\Role;
-
 use App\Models\Day;
-
 class ConstantController extends Controller
 {
-    public function index()
-    {
+public function index(){
+    
+ $doctors_id = Role::where('name','Doctor')->first()->id;
 
-        $doctors_id = Role::where('name', 'Doctor')->first()->id;
-
-        $doctors = User::where('role_id', $doctors_id)->get();
-
-
-        $constants = Constant_settings::all();
-        return view('admin.pages.constant_settings', [
-            'constants' => $constants,
-            'doctors' => $doctors
-        ]);
-    }
-
-    public function delete_time($id)
-    {
-        Day::find($id)->delete();
-        return back();
-    }
-
-    public function add_time(Request $request)
-    {
-        $request->validate([
-            'day' => 'required',
-            'duration' => 'nullable',
-            'availableTime' => 'required',
-            'default' => 'nullable',
-            'closedTime' => 'required',
-            'constant_setting_id' => 'required',
-        ]);
-//        dd($request->all());
-
-        if (Day::where('constant_setting_id', $request->constant_setting_id)->where('AvailableDate', $request->day)->exists()) {
-
-            return back()->with('exists', $request->day . 'available time already given');
-
-        } else {
-            Day::insert([
-                'constant_setting_id' => $request->constant_setting_id,
-                'duration' => $request->duration ?? $request->default,
-                'availableTime' => $request->availableTime,
-                'closedTime' => $request->closedTime,
-                'AvailableDate' => $request->day,
-            ]);
-            return back();
-        }
+ $doctors = User::where('role_id',$doctors_id)->get();
 
 
-        $constants = Constant_settings::all();
-        return view('admin.pages.constant_settings', [
-            'constants' => $constants,
-            'doctors' => $doctors
-        ]);
-    }
+    $constants = Constant_settings::all();
+    return view('admin.pages.constant_settings',[
+        'constants'=>$constants,
+        'doctors'=>$doctors
+    ]);
+}
+public function delete_time($id){
+    Day::find($id)->delete();
+    return back();
+}
+public function add_time(Request $request){
 
 
-    public function add(Request $request)
-    {
-//        dd($request->all());
-        $request->validate([
-            'doctor_id' => 'required',
-            'speciality' => 'required',
-            'price' => 'required',
-            'duration' => 'required',
-        ]);
-        if (Constant_settings::where('user_id', $request->doctor_id)->exists()) {
-            return back()->with('exists', 'This doctor had been already assigned');
-        } else {
-            Constant_settings::insert([
-                'user_id' => $request->doctor_id,
-                'speciality' => $request->speciality,
-                'price' => $request->price,
-                'duration' => $request->price
-            ]);
-        }
 
-        return back();
+$request->validate([
+    'day'=>'required',
+    'duration'=>'required',
+    'availableTime'=>'required',
+    'closedTime'=>'required',
+]);
 
 
-        $constants = Constant_settings::all();
-        return view('admin.pages.constant_settings', [
-            'constants' => $constants,
-            'doctors' => $doctors
-        ]);
-    }
-
-//    public function add(Request $request)
-//    {
-//        $request->validate([
-//            'doctor_id' => 'required',
-//            'availableTime' => 'required',
-//            'speciality' => 'required',
-//            'price' => 'required',
-//        ]);
-//// dd($request->all());
-//        Constant_settings::insert([
-//            'user_id' => $request->doctor_id,
-//            'availableTime' => $request->availableTime,
-//            'speciality' => $request->speciality,
-//            'price' => $request->price
-//        ]);
-//        return back();
-//
-//
-//    }
-
-    public function delete(Request $request)
-    {
-        Constant_settings::find($request->id)->delete();
-        return back();
-    }
 
 
-    public function update(Request $request)
-    {
+if(Day::where('constant_id',$request->constant_id)->where('AvailableDate',$request->day)->exists()){
 
+    return back()->with('exists',$request->day.'available time already given');
 
-        $request->validate([
-            'doctor_id' => 'required',
-            'availableTime' => 'required',
-            'speciality' => 'required',
-            'price' => 'required',
-        ]);
-
-        Constant_settings::find($request->id)->update([
-            'user_id' => $request->doctor_id,
-            'availableTime' => $request->availableTime,
-            'speciality' => $request->speciality,
-            'price' => $request->price,
+}else{
+    
+    $start_time = $request->availableTime;
+    $end_time = $request->closedTime;
+    $duration = $request->duration;
+    
+    $start_time_break = explode(':',$start_time);
+    $start_mins = ($start_time_break[0]*60) + $start_time_break[1];
+    $end_time_break = explode(':',$end_time);
+    $end_mins = ($end_time_break[0]*60) + $end_time_break[1];
+    
+    $total_mins = $end_mins - $start_mins;
+    
+    $doctor_total_consult_will_be = $total_mins / $duration;
+    if($doctor_total_consult_will_be < 1 || is_float($doctor_total_consult_will_be)){
+        return back()->with('exists','Your time is invalid duration is not mathched with given time?');
+      
+    }else{
+        Day::insert([
+            'constant_id'=>$request->constant_id,
+            'duration'=>$request->duration,
+            'availableTime'=>$request->availableTime,
+            'closedTime'=>$request->closedTime,
+            'AvailableDate'=>$request->day,
         ]);
         return back();
-
-
     }
+    
+
+
+}
+
+}
+public function add(Request $request){
+    $request->validate([
+   'doctor_id'=>'required',
+   'speciality'=>'required',
+   'price'=>'required',
+    ]);
+if(Constant_settings::where('user_id',$request->doctor_id)->exists()){
+ return back()->with('exists','This doctor had been already assigned');
+}else{
+    Constant_settings::insert([
+        'user_id'=>$request->doctor_id,
+        'speciality'=>$request->speciality,
+        'price'=>$request->price
+        ]);
+}
+
+   
+return back();
+
+}
+public function delete(Request $request){
+    Constant_settings::find($request->id)->delete();
+    return back();
+}
+public function update(Request $request){
+
+
+    $request->validate([
+        'doctor_id'=>'required',
+        'speciality'=>'required',
+        'price'=>'required',
+         ]);
+
+  Constant_settings::find($request->id)->update([
+ 'user_id'=>$request->doctor_id,
+ 'speciality'=>$request->speciality,
+ 'price'=>$request->price,
+  ]);
+  return back();
+
+
+}
 }
